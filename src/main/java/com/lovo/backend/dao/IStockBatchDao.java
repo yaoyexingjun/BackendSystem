c88a0f1566872bd8e0cd8e5afc62b9d424197f8d
Yangbin970710 <840777192@qq.com>
2020-02-13 14:56:32 +0800

    库存批次显示业务

--- src/main/java/com/lovo/backend/dao/IStockBatchDao.java
+++ src/main/java/com/lovo/backend/dao/IStockBatchDao.java
@@ -1,6 +1,7 @@
 package com.lovo.backend.dao;
 
 import com.lovo.backend.entity.StockBatchEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,12 +17,20 @@ public interface IStockBatchDao extends CrudRepository<StockBatchEntity,Long> {
      * @return 库存批次集合
      */
     @Query("from StockBatchEntity sbe order by sbe.batch asc")
-    public List<StockBatchEntity> findAllStockBatch();
+    public List<StockBatchEntity> findAllStockBatch(Pageable pageable);
 
     /**
      * 根据供货批次查询全部库存批次
      * @return 库存批次集合
      */
     @Query("from StockBatchEntity sbe where sbe.batch = :supplyBatch")
-    public List<StockBatchEntity> findStockBatchBySupplyBatch(@Param("supplyBatch")String supplyBatch);
+    public List<StockBatchEntity> findStockBatchBySupplyBatch(@Param("supplyBatch")String supplyBatch,Pageable pageable);
+
+    /**
+     * 根据供货批次查询库存批次记录数
+     * @param supplyBatch 供货批次
+     * @return 库存批次记录数
+     */
+    @Query("select count(sbe.stockBatchId) from StockBatchEntity sbe where sbe.batch = :supplyBatch")
+    public int findAllCountBySupplyBatch(@Param("supplyBatch")String supplyBatch);
 }
--- src/main/java/com/lovo/backend/service/impl/StockBatchServiceImpl.java
+++ src/main/java/com/lovo/backend/service/impl/StockBatchServiceImpl.java
@@ -4,6 +4,7 @@ import com.lovo.backend.dao.IStockBatchDao;
 import com.lovo.backend.entity.StockBatchEntity;
 import com.lovo.backend.service.IStockBatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,12 +23,22 @@ public class StockBatchServiceImpl implements IStockBatchService {
         stockBatchDao.save(stockBatchEntity);
     }
 
-    public List<StockBatchEntity> findAllStockBatch() {
-        return stockBatchDao.findAllStockBatch();
+    public List<StockBatchEntity> findAllStockBatch(Pageable pageable) {
+        return stockBatchDao.findAllStockBatch(pageable);
     }
 
-    public List<StockBatchEntity> findStockBatchBySupplyBatch(String supplyBatch) {
-        return stockBatchDao.findStockBatchBySupplyBatch(supplyBatch);
+    public int getTotalCount(String supplyBatch) {
+        return stockBatchDao.findAllCountBySupplyBatch(supplyBatch);
+    }
+
+    public int findAllPage(String supplyBatch) {
+        int count = this.getTotalCount(supplyBatch);
+        int totalPage = (count + 5 - 1) / 5;
+        return totalPage;
+    }
+
+    public List<StockBatchEntity> findStockBatchBySupplyBatch(String supplyBatch,Pageable pageable) {
+        return stockBatchDao.findStockBatchBySupplyBatch(supplyBatch,pageable);
     }
 
 }
